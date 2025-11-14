dda28ab1e5b9c92657fe00d3fb6746f7960487a4
KnoxNeon <iftey10@gmail.com>
2025-11-14 10:37:02 +0600

    add footer

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import { Outlet } from 'react-router'
+import Footer from '../Components/Footer'
 
 const MainLayout = () => {
   return <>
     <Navbar/>
     <Outlet/>
+    <Footer/>
   </>
 }
 
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,18 +1,20 @@
 import { createBrowserRouter } from "react-router";
 import Apps from "../Pages/Apps";
 import MainLayout from "../Layouts/MainLayout";
+import ErrorPage from "../Pages/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout/>,
+    errorElement: <ErrorPage/>,
     children: [
         {
             path: "/apps",
             element: <Apps />,
         },
-
     ]
+    
   },
  
 ]);
