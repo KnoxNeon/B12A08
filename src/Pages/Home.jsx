611e3f0a971214b5ea880d34d9dec18b97a865bb
KnoxNeon <iftey10@gmail.com>
2025-11-22 23:37:30 +0600

    add fix footer for md screen

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -12,8 +12,8 @@ const Home = () => {
 
     <div>
       <div><Hero/><Banner/></div>
-      <div ><h1 className='mt-4 lg:mt-20 mb-2 text-3xl lg:text-5xl font-bold text-center'>Trending Apps</h1>
-      <p className='text-[#627382] text-md lg:text-xl text-center mb-10'>Explore All Trending Apps on the Market developed by us</p>
+      <div ><h1 className='pt-4 lg:mt-20 mb-2 text-3xl lg:text-5xl font-bold text-center'>Trending Apps</h1>
+      <p className='text-[#627382] text-md lg:text-xl text-center pb-10'>Explore All Trending Apps on the Market developed by us</p>
       <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-6 lg:my-10 m-6 lg:mx-20'>
         {
           featuredApps.map(app =>(
@@ -21,7 +21,7 @@ const Home = () => {
           ))
         }
       </div>
-      <div className="flex justify-center"><Link className='btn btn-ghost  text-white bg-linear-to-r from-[#632EE3] to-[#9F62F2] px-8 mb-10' to='/apps'>See All</Link></div></div>
+      <div className="flex justify-center pb-10"><Link className='btn btn-ghost  text-white bg-linear-to-r from-[#632EE3] to-[#9F62F2] px-8 ' to='/apps'>See All</Link></div></div>
       
       
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router';
 const Hero = () => {
   return (
     <div className="flex flex-col justify-center text-center w-full mx-auto bg-[#f3f3f3] ">
-      <h1 className='text-[#001931] text-3xl md:text-7xl font-bold text-shadow-2xl mt-10 md:mt-14'>We Build <br /> <span className='bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent'>Productive</span> Apps</h1>
+      <h1 className='text-[#001931] text-3xl md:text-7xl font-bold text-shadow-2xl pt-10 md:pt-14'>We Build <br /> <span className='bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent'>Productive</span> Apps</h1>
       <p className='text-[#627382] text-md md:text-xl line-clamp-2 mt-4 lg:mt-6'>At HERO.IO, we craft innovative apps designed to make everyday life simpler, smarter, and more exciting.<br/> Our goal is to turn your ideas into digital experiences that truly make an impact.</p>
       <div className='space-x-4 my-6 lg:my-10'>
         <Link to="https://play.google.com/store/games?hl=en" className=' btn'> <IoLogoGooglePlaystore />Google Play</Link>
